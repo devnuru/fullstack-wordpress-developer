9da01546d79b903fb379e320f74c19fff7b22787
Devnuru <devnuruzzaman@gmail.com>
2024-06-20 14:00:05 +0600

    Live search feature

--- wp-content/themes/fictional-university-theme/build/index.asset.php
+++ wp-content/themes/fictional-university-theme/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '99e84003d96a4396c506');
+<?php return array('dependencies' => array('jquery'), 'version' => 'bec1b1680c3f3fe09f57');
--- wp-content/themes/fictional-university-theme/build/index.js
+++ wp-content/themes/fictional-university-theme/build/index.js
@@ -198,11 +198,17 @@ __webpack_require__.r(__webpack_exports__);
 class Search {
   // 1. describe and create/initiate our object
   constructor() {
+    this.addSearchHTML();
+    this.resultsDiv = jquery__WEBPACK_IMPORTED_MODULE_0___default()("#search-overlay__results");
     this.openButton = jquery__WEBPACK_IMPORTED_MODULE_0___default()(".js-search-trigger");
     this.closeButton = jquery__WEBPACK_IMPORTED_MODULE_0___default()(".search-overlay__close");
     this.searchOverlay = jquery__WEBPACK_IMPORTED_MODULE_0___default()(".search-overlay");
+    this.searchField = jquery__WEBPACK_IMPORTED_MODULE_0___default()("#search-term");
     this.events();
     this.isOverlayOpen = false;
+    this.isSpinnerVisible = false;
+    this.previousValue;
+    this.typingTimer;
   }
 
   // 2. events
@@ -210,17 +216,91 @@ class Search {
   events() {
     this.openButton.on("click", this.openOverlay.bind(this));
     this.closeButton.on("click", this.closeOverlay.bind(this));
-    jquery__WEBPACK_IMPORTED_MODULE_0___default()(document).on("keyup", this.keyPressDispatcher.bind(this));
+    jquery__WEBPACK_IMPORTED_MODULE_0___default()(document).on("keydown", this.keyPressDispatcher.bind(this));
+    this.searchField.on("keyup", this.typingLogic.bind(this));
   }
 
   // 3. methods (function, action....)
 
+  typingLogic() {
+    // alert("hello from typng logic");
+    if (this.searchField.val() != this.previousValue) {
+      clearTimeout(this.typingTimer);
+      if (this.searchField.val()) {
+        if (!this.isSpinnerVisible) {
+          this.resultsDiv.html('<div class="spinner-loader"></div>');
+          this.isSpinnerVisible = true;
+        }
+        this.typingTimer = setTimeout(this.getResults.bind(this), 1000);
+      } else {
+        this.resultsDiv.html("");
+        this.isSpinnerVisible = false;
+      }
+    }
+    this.previousValue = this.searchField.val();
+  }
+  getResults() {
+    jquery__WEBPACK_IMPORTED_MODULE_0___default().when(jquery__WEBPACK_IMPORTED_MODULE_0___default().getJSON(universityData.root_url + "/wp-json/wp/v2/posts?search=" + this.searchField.val()), jquery__WEBPACK_IMPORTED_MODULE_0___default().getJSON(universityData.root_url + "/wp-json/wp/v2/pages?search=" + this.searchField.val())).then((posts, pages) => {
+      var combinedResults = posts[0].concat(pages[0]);
+      this.resultsDiv.html(`
+          <h2 class="search-overlay__section-title">General Information</h2>
+         ${combinedResults.length ? '<ul class="link-list min-list">' : "<p>No general information matches that search.</p>"}
+          ${combinedResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</a> ${item.type == "post" ? `by ${item.authorName}` : ""}</li>`).join("")}
+          ${combinedResults.length ? "</ul>" : ""}
+          `);
+      this.isSpinnerVisible = false;
+    }, () => {
+      this.resultsDiv.html("<p>Unexpected error: please try again.</p>");
+    });
+
+    //   $.getJSON(
+    //     universityData.root_url +
+    //       "/wp-json/wp/v2/posts?search=" +
+    //       this.searchField.val(),
+    //     (posts) => {
+    //       $.getJSON(
+    //         universityData.root_url +
+    //           "/wp-json/wp/v2/pages?search=" +
+    //           this.searchField.val(),
+    //         (pages) => {
+    //           var combinedResults = posts.concat(pages);
+    //           this.resultsDiv.html(`
+    //         <h2 class="search-overlay__section-title">General Information</h2>
+    //        ${
+    //          combinedResults.length
+    //            ? '<ul class="link-list min-list">'
+    //            : "<p>No general information matches that search.</p>"
+    //        }
+    //         ${combinedResults
+    //           .map(
+    //             (item) =>
+    //               `<li><a href="${item.link}">${item.title.rendered}</a></li>`
+    //           )
+    //           .join("")}
+    //         ${combinedResults.length ? "</ul>" : ""}
+    //         `);
+    //           this.isSpinnerVisible = false;
+    //         }
+    //       );
+    //     }
+    //   );
+  }
+
+  // getResults() {
+  //   fetch(
+  //     `http://fictionaluniversity.local/wp-json/wp/v2/posts/?search=${searchField.value}`
+  //   )
+  //     .then((res) => res.json())
+  //     .then((out) => {
+  //       console.log("Output: ", out[0].title.rendered);
+  //     })
+  //     .catch((err) => console.error(err));
+  // }
+
   keyPressDispatcher(e) {
     // console.log(e.keyCode);
 
-    if (e.keyCode == 83 && !this.isOverlayOpen) {
-      this.openOverlay();
-    }
+    if (e.keyCode == 83 && !this.isOverlayOpen && !jquery__WEBPACK_IMPORTED_MODULE_0___default()("input, textarea").is(":focus")) this.openOverlay();
     if (e.keyCode == 27 && this.isOverlayOpen) {
       this.closeOverlay();
     }
@@ -228,6 +308,8 @@ class Search {
   openOverlay() {
     this.searchOverlay.addClass("search-overlay--active");
     jquery__WEBPACK_IMPORTED_MODULE_0___default()("body").addClass("body-no-scroll");
+    setTimeout(() => this.searchField.focus(), 300);
+    this.searchField.val("");
     console.log("Our open method ran!");
     this.isOverlayOpen = true;
   }
@@ -236,6 +318,24 @@ class Search {
     jquery__WEBPACK_IMPORTED_MODULE_0___default()("body").removeClass("body-no-scroll");
     console.log("Our close method ran!");
     this.isOverlayOpen = false;
+  }
+  addSearchHTML() {
+    jquery__WEBPACK_IMPORTED_MODULE_0___default()("body").append(`
+        <div class="search-overlay">
+    <div class="search-overlay__top">
+      <div class="container">
+        <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+        <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term" autocomplete="off">
+        <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+      </div>
+    </div>
+
+       <div class="container">
+        <div id="search-overlay__results"></div>
+       </div>
+
+  </div>
+      `);
   }
 }
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Search);
